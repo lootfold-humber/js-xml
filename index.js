c75dacc25f66738720597dbadd0ffbac2476f788
Pallav Dubey <pallav.dubey@gmail.com>
2022-02-09 18:10:14 -0500

    add layout for home page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.port || "3000";
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (request, response) => {
+  response.render("index", {
+    title: "Grocery Store",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
