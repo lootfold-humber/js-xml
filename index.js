1ed3d6051a3b24ed394e6db1194d17eb1b434438
Pallav Dubey <pallav.dubey@gmail.com>
2022-03-23 21:03:17 -0400

    add omdb api call

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+
+const axios = require("axios");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
 const port = process.env.port || "3000";
 
+const path = require("path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -15,11 +24,20 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/search", (req, res) => {
+app.get("/search", async (req, res) => {
   const { search_key } = req.query;
-  console.log(search_key);
+  const data = await getSearchResults(search_key);
+  console.log(data);
+  // res.render("results", data);
 });
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
+async function getSearchResults(searchKey) {
+  const omdbBaseUrl = process.env.OMDB_URL;
+  const omdbApiKey = process.env.OMDB_API_KEY;
+  const url = `${omdbBaseUrl}/?apikey=${omdbApiKey}&s="${searchKey}"`;
+
+  const { data } = await axios.get(url);
+  const { Search: search } = data;
+
+  return search;
+}
